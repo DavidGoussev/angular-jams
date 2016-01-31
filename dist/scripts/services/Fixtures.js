027fcf4a84a5eeba0360ebe114b01a6579c927f4
dpg5000 <dgoussev@gmail.com>
2016-01-30 16:22:43 -0800

    stash local files

---
+++ dist/scripts/services/Fixtures.js
@@ -0,0 +1,46 @@
+(function() {
+    function Fixtures() {
+        var Fixtures = {};
+
+        var albumPicasso = {
+            name: 'Parklife',
+            artist: 'Blur',
+            label: 'Parlophone',
+            year: '1994',
+            albumArtUrl: '/assets/images/album_covers/01.png',
+            songs: [
+                { name: 'Girls and Boys', length: 161.71, audioUrl: '/assets/music/blue' },
+                { name: 'Tracey Jacks', length: 103.96, audioUrl: '/assets/music/green' },
+                { name: 'Badhead', length: 268.45, audioUrl: '/assets/music/red' },
+                { name: 'To The End', length: 153.14, audioUrl: '/assets/music/pink' },
+                { name: 'London Loves', length: 374.22, audioUrl: '/assets/music/magenta' }
+            ]
+        };
+
+        var albumMarconi = {
+            name: 'The Telephone',
+            artist: 'Guglielmo Marconi',
+            label: 'EM',
+            year: '1909',
+            albumArtUrl: '/assets/images/album_covers/20.png',
+            songs: [
+                { name: 'Hello, Operator?', length: '1:01' },
+                { name: 'Ring, ring, ring', length: '5:01' },
+                { name: 'Fits in your pocket', length: '3:21' },
+                { name: 'Can you hear me now?', length: '3:14' },
+                { name: 'Wrong phone number', length: '2:15' }
+            ]
+        };
+        
+        Fixtures.getAlbum = function() {
+            return albumPicasso;
+        };
+        
+        return Fixtures;
+    }
+
+    angular
+        .module('angularJams')
+        .factory('Fixtures', Fixtures);
+
+})();
